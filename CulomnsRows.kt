c5c8a1a93724951831ac50af2c2010f4ed319664
Diivineee <divinedesamparado23@gmail.com>
2022-08-27 21:20:37 +0700

    init

---
+++ CulomnsRows.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // Write your code below
+    
+    for (i in 1..6) {
+      for (j in 'A'..'F') {
+        print("$j$i ")
+      }
+  
+      println()
+  
+    }
+  }
---
+++ HelloCodey.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // Write your code below
+   var num = 5
+    for(i in 1..5){
+  
+      println("Hello, Codey!")
+   println("i=$i")
+    }
+  for (i in 1..num){
+   
+  
+  
+    }
+  }
